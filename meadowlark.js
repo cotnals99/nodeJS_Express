396baa2120705046ea9f1d3a54989ee46bd78a1f
cotnals <su.chae@icloud.com>
2022-05-04 17:14:55 +0700

    Add handler, ESlint, puppeteer, jest, portfinder

--- meadowlark.js
+++ meadowlark.js
@@ -3,14 +3,8 @@ const expressHandlebars = require('express-handlebars')
 
 const app = express()
 
-
-const fortunes = [
-  'Conquer your fears or they will conquer you.',
-  'Rivers need springs.',
-  "Do not fear what you don't know.",
-  'You will have a pleasant surprise.',
-  'Whenever possible, keep it simple',
-]
+const handlers = require('./lib/handlers')
+// const fortune = require('./lib/fortunes')
 
 // configure Handlebars view engine
 app.engine('handlebars', expressHandlebars({
@@ -22,29 +16,26 @@ app.use(express.static(__dirname + '/public'))
 
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => res.render('home'))
+app.get('/', handlers.home)
 
-app.get('/about', (req, res) => {
-  const randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)]
-  res.render('about', {fortune:randomFortune})
-})
+app.get('/about', handlers.about)
 
 // custom 404 page
-app.use((req, res) => {
-  res.status(404)
-  res.render('404')
-})
+app.use(handlers.notFound)
 
 // custom 500 page
-app.use((err, req, res, next) => {
-  console.error(err.message)
-  res.status(500)
-  res.render('500')
-})
+app.use(handlers.serverError)
 
-app.listen(port, () => console.log(
-  `Express started on http://localhost:${port}; ` +
-  `press Ctrl-C to terminate.`))
+// app.listen(port, () => console.log(
+//   `Express started on http://localhost:${port}; ` +
+//   `press Ctrl-C to terminate.`))
 
 
-
+if(require.main === module) {
+  app.listen(port, ()=>{
+    console.log(`Express started on http://localhost:${port}` + 
+    'press Ctrl-C to terminate.')
+  })
+} else {
+  module.exports = app
+}
