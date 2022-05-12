7dfb936cc683481bc310b741342e5762757c999b
cotnals <su.chae@icloud.com>
2022-05-12 13:51:22 +0700

    handlers, signup,  multiparty upload

--- meadowlark.js
+++ meadowlark.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const expressHandlebars = require('express-handlebars')
 const weatherMiddlware = require('./lib/middleware/weather')
+const bodyParser = require('body-parser')
+const multiparty = require('multiparty')
 
 const app = express()
 
@@ -22,6 +24,8 @@ app.set('view engine', 'handlebars')
 
 app.use(express.static(__dirname + '/public'))
 
+app.use(bodyParser.urlencoded({extended: true}))
+
 const port = process.env.PORT || 3000
 
 
@@ -30,6 +34,38 @@ app.use(weatherMiddlware)
 app.get('/', handlers.home)
 app.get('/about', handlers.about)
 app.get('/section-test', handlers.sectionTest)
+
+
+// handlers for browser-based form submission
+app.get('/newsletter-signup', handlers.newsletterSignup)
+app.post('/newsletter-signup/process', handlers.newsletterSignupProcess)
+app.get('/newsletter-signup/thank-you', handlers.newsletterSignupThankYou)
+
+// handlers for fetch/JSON form submission
+app.get('/newsletter', handlers.newsletter)
+app.post('/api/newsletter-signup', handlers.api.newsletterSignup)
+
+
+// vacation photo contest
+app.get('/contest/vacation-photo', handlers.vacationPhotoContest)
+app.get('/contest/vacation-photo-ajax', handlers.vacationPhotoContestAjax)
+app.post('/contest/vacation-photo/:year/:month', (req, res) => {
+  const form = new multiparty.Form()
+  form.parse(req, (err, fields, files) => {
+    if(err) return handlers.vacationPhotoContestProcessError(req, res, err.message)
+    console.log('got fields: ', fields)
+    console.log('and files: ', files)
+    handlers.vacationPhotoContestProcess(req, res, fields, files)
+  })
+})
+app.get('/contest/vacation-photo-thank-you', handlers.vacationPhotoContestProcessThankYou)
+app.post('/api/vacation-photo-contest/:year/:month', (req, res) => {
+  const form = new multiparty.Form()
+  form.parse(req, (err, fields, files) => {
+    if(err) return handlers.api.vacationPhotoContestError(req, res, err.message)
+    handlers.api.vacationPhotoContest(req, res, fields, files)
+  })
+})
 
 app.use(handlers.notFound)
 app.use(handlers.serverError)
